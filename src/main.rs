26e63ed8d0d834154ec993a2c512fdc2b569d726
Bradford D. Boyle <bradford.d.boyle@gmail.com>
2017-03-12 16:37:43 -0700

    Basic record parsing

--- src/main.rs
+++ src/main.rs
@@ -14,7 +14,7 @@ use pnet::packet::ipv4::Ipv4Packet;
 use pnet::packet::ip::IpNextHeaderProtocol;
 
 
-#[derive(Debug)]
+#[derive(Debug, Default)]
 struct Header {
     version: u16,
     count: u16,
@@ -25,6 +25,63 @@ struct Header {
     engine_type: u8,
     engine_id: u8,
     sampling_interval: u16
+}
+
+#[derive(Debug, Default)]
+struct Record {
+    src_addr: u32,
+    dst_addr: u32,
+    next_hop: u32,
+    input: u16,
+    output: u16,
+    d_pkts: u32,
+    d_octets: u32,
+    first: u32,
+    last: u32,
+    src_port: u16,
+    dst_port: u16,
+    tcp_flags: u8,
+    prot: u8,
+    tos: u8,
+    src_as: u16,
+    dst_as: u16,
+    src_mask: u8,
+    dst_mask: u8
+}
+
+
+
+impl Record {
+    pub fn new(data: &[u8]) -> Record {
+        let mut rdr = std::io::Cursor::new(data);
+
+        let mut r: Record = Default::default();
+        r.src_addr = rdr.read_u32::<BigEndian>().unwrap();
+        r.dst_addr = rdr.read_u32::<BigEndian>().unwrap();
+        r.next_hop = rdr.read_u32::<BigEndian>().unwrap();
+        r.input = rdr.read_u16::<BigEndian>().unwrap();
+        r.output = rdr.read_u16::<BigEndian>().unwrap();
+        r.d_pkts = rdr.read_u32::<BigEndian>().unwrap();
+        r.d_octets = rdr.read_u32::<BigEndian>().unwrap();
+        r.first = rdr.read_u32::<BigEndian>().unwrap();
+        r.last = rdr.read_u32::<BigEndian>().unwrap();
+        r.src_port = rdr.read_u16::<BigEndian>().unwrap();
+        r.dst_port = rdr.read_u16::<BigEndian>().unwrap();
+
+        // byte 36 is a padding byte
+        let cur_pos = rdr.position();
+        rdr.set_position(cur_pos + 1);
+
+        r.tcp_flags = rdr.read_u8().unwrap();
+        r.prot = rdr.read_u8().unwrap();
+        r.tos = rdr.read_u8().unwrap();
+        r.src_as = rdr.read_u16::<BigEndian>().unwrap();
+        r.dst_as = rdr.read_u16::<BigEndian>().unwrap();
+        r.src_mask = rdr.read_u8().unwrap();
+        r.dst_mask = rdr.read_u8().unwrap();
+
+        Record { ..r}
+    }
 }
 
 
@@ -90,6 +147,12 @@ fn handle_netflow(d: &[u8]) {
         engine_id: rdr.read_u8().unwrap(),
         sampling_interval: rdr.read_u16::<BigEndian>().unwrap(),
     };
+    println!("{:?}", hdr);
 
-    println!("header = {:?}", hdr);
+    for i in 0..hdr.count as usize {
+        let start = 24 + i * 48;
+        let end = start + 48;
+        let r = Record::new(&d[start..end]);
+        println!("\t{:?}", r);
+    }
 }
